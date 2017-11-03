ed932626cc76943e66a5d4dacc183b47a365e0c5
Gray-Young <1162032953@qq.com>
2017-11-03 22:24:06 +0800

    添加动态

--- application/index/controller/Dynimic.php
+++ application/index/controller/Dynimic.php
@@ -2,11 +2,30 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
+use think\Request;
 
 class Dynimic extends Controller
 {
-    public function index()
+    public function show_add_dynimic()
     {
-
+        $members = Db::query('SELECT * FROM member');
+        $this->assign('members', $members);
+        return $this->fetch();
+    }
+    public function show_dynimic_list()
+    {
+        $dynimics = Db::query('SELECT * FROM Dynimic d JOIN member c WHERE d.Dynimic_member=c.member_no');
+        $this->assign('dynimics', $dynimics);
+        return $this->fetch();
+    }
+    public function add_dynimic()
+    {
+        $postData = Request::instance() -> post();
+        Db::execute('INSERT INTO Dynimic
+            (Dynimic_member, Dynimic_count, Dynimic_type, Dynimic_remark, Dynimic_datatime) VALUES
+            (?, ?, ?, ?, ?)',
+            [$postData['Dynimic_member'], $postData['Dynimic_count'], $postData['Dynimic_type'], $postData['Dynimic_remark'], date('Y-m-d H:i:s')]);
+        return $this->success("添加成功");
     }
 }
--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -11,16 +11,63 @@ class Member extends Controller
     {
         return $this->fetch();
     }
-    public function add_member()
-    {
-        $postData = Request::instance() -> post();
-        Db::execute('insert into member (member_name, member_sex, member_birthday) values (?, ?, ?)',[$postData['member_name'], $postData['member_sex'], $postData['member_birthday']]);
-        return $this->success("添加成功");
-    }
+
     public function show_member_list()
     {
         $members = Db::query('select * from member');
         $this->assign('members', $members);
         return $this->fetch();
+    }
+
+    public function show_modify_member($member_no)
+    {
+        $members = Db::query('SELECT * FROM member WHERE member_no = ' . $member_no . ';');
+        $this->assign('members', $members);
+        return $this->fetch();
+    }
+
+    public function show_member_detial($member_no)
+    {
+        $members = Db::query('SELECT * FROM member WHERE member_no = ' . $member_no . ';');
+        $this->assign('members', $members);
+        return $this->fetch();
+    }
+
+    public function add_member()
+    {
+        $postData = Request::instance() -> post();
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('member_photo');
+
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+            if($info){
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                echo $info->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getSaveName();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                echo $info->getFilename();
+            }else{
+                // 上传失败获取错误信息
+                echo $file->getError();
+            }
+        }
+        Db::execute('INSERT INTO member
+            (member_name, member_sex, member_birthday, member_company, member_job, member_nation, member_photo) VALUES
+            (?, ?, ?, ?, ?, ?, ?)',
+            [$postData['member_name'], $postData['member_sex'], $postData['member_birthday'], $postData['member_company'], $postData['member_job'], $postData['member_nation'], $info->getSaveName()]);
+        return $this->success("添加成功");
+    }
+
+    public function delete_member($member_no)
+    {
+        $sql = "DELETE FROM member WHERE member_no = " . $member_no .";";
+        Db::execute($sql);
+        $sql1 = "DELETE FROM Dynimic WHERE Dynimic_member = " . $member_no .";";
+        Db::execute($sql1);
+        return $this->success("删除成功");
     }
 }
