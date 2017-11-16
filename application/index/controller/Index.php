640e76abb4ede9c84c74ed29febf1135f609d5cd
Gray-Young <1162032953@qq.com>
2017-11-16 20:55:18 +0800

    转移主力机

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,14 +2,28 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\Model;
+use think\Request;
+use think\Db;
 
 class Index extends Controller
 {
+    //加载登录界面
     public function index()
     {
-
         return $this->fetch();
+    }
+    public function login()
+    {
+        $postData = Request::instance() -> post();
+        $members = Db::query("SELECT `member_password` FROM member WHERE member_name = '" . $postData['name'] . "';");
+        if($members[0]['member_password'] == $postData['password'])
+        {
+            $this->redirect('/family/public/index/Dynimic/show_dynimic_list');
+        }
+        else
+        {
+            return $this->error("登录失败");
+        }
     }
 
 }
